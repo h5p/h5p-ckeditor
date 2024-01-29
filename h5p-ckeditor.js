4127929098a6ad99869847605bf8ddcd20c9c2e5
makmentins <maris.akmentins@h5p.group>
2024-01-29 08:47:03 +0100

    JI-5219 add h4-h6, pre, address and div styles to default cfg

--- h5p-ckeditor.js
+++ h5p-ckeditor.js
@@ -7,6 +7,27 @@ H5P.CKEditor = (function (EventDispatcher, $) {
     toolbar: [
       'heading', '|', 'bold', 'italic', 'underline', 'strikeThrough', '|', 'link', '|', 'insertTable'
     ],
+    heading: {
+      options: [
+        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+        { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+        { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+        { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
+        { model: 'formatted', view: 'pre', title: 'Formatted' },
+        { model: 'address', view: 'address', title: 'Address' },
+        { model: 'normal', view: 'div', title: 'Normal (DIV)' }
+      ]
+    },
+    contentCss: {
+      '.ck': {
+        'background-color': 'green !important',
+        'max-height': '40%',
+        'overflow-y': 'auto'
+      }
+    },
     table: {
       contentToolbar: [
         'toggleTableCaption',
